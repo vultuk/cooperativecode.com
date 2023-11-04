415516322420c135aba7e138de88bb235061eb69
Simon Skinner <vultuk28@gmail.com>
2023-11-04 12:11:24 +0000

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.6.2
      project name = cooperative-code
      framework = next
      framework cli = create-next-app@13.4.19
      package manager = npm@9.5.1
      wrangler = wrangler@3.15.0
      git = 2.40.1

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+// Next.js Edge API Routes: https://nextjs.org/docs/pages/building-your-application/routing/api-routes#edge-api-routes
+
+import type { NextRequest } from 'next/server'
+
+export const config = {
+  runtime: 'edge',
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextRequest) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
 }
